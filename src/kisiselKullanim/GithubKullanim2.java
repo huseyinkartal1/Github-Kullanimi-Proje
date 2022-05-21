3f5755c0610bd9fe7f5882a369481eb4649d66e7
bulutluoz <abbulutluoz@gmail.com>
2022-05-21 12:02:22 +0200

    ilk

---
+++ src/kisiselKullanim/GithubKullanim2.java
@@ -0,0 +1,23 @@
+package kisiselKullanim;
+
+public class GithubKullanim2 {
+
+
+    public void method1() {
+        System.out.println("main branch");
+
+
+    }
+    public static void main (String[]args){
+
+        System.out.println("branch main");
+        System.out.println("deneme");
+
+        System.out.println("main ilave");
+        System.out.println("deneme ek");
+
+
+    }
+}
+
+
